141d23133423bd9ae8f1b83553aec4e683bcf592
Adrien Grandemange <itsmeadri@gmail.com>
2021-04-10 02:39:26 +0200

    Initialize logical device with proper extensions

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use std::{
 };
 
 use ash::{
-    version::{EntryV1_0, InstanceV1_0},
+    version::{DeviceV1_0, EntryV1_0, InstanceV1_0},
     vk::Handle,
 };
 use libloading::Library;
@@ -141,31 +141,10 @@ pub fn android_main() {
         xr_available_extensions
     );
 
-    let required_layers: &[&str] = &[];
+    let required_layers = to_veccstr(&[]);
 
-    // Create NULL-terminated CStrings and collect pointers to them
-    // into vectors to be passed to OpenXR.
-    let required_layers_cstring = required_layers
-        .iter()
-        .filter_map(|&name| CString::new(name).ok())
-        .collect::<Vec<_>>();
-
-    let required_layer_ptrs = required_layers_cstring
-        .iter()
-        .map(|layer| layer.as_ptr())
-        .collect::<Vec<_>>();
-
-    let required_extensions = ["XR_KHR_vulkan_enable", "XR_KHR_android_create_instance"];
-
-    let required_exts_cstring = required_extensions
-        .iter()
-        .filter_map(|&name| CString::new(name).ok())
-        .collect::<Vec<_>>();
-
-    let required_ext_ptrs = required_exts_cstring
-        .iter()
-        .map(|layer| layer.as_ptr())
-        .collect::<Vec<_>>();
+    let required_extensions =
+        to_veccstr(&["XR_KHR_vulkan_enable", "XR_KHR_android_create_instance"]);
 
     // https://www.khronos.org/registry/OpenXR/specs/1.0/html/xrspec.html#XR_KHR_android_create_instance
     let create_info_ext = Box::into_raw(Box::new(openxr_sys::InstanceCreateInfoAndroidKHR {
@@ -180,10 +159,10 @@ pub fn android_main() {
         next: create_info_ext,
         create_flags: openxr_sys::InstanceCreateFlags::EMPTY,
         application_info,
-        enabled_api_layer_count: required_layer_ptrs.len() as _,
-        enabled_api_layer_names: required_layer_ptrs.as_ptr(),
-        enabled_extension_count: required_ext_ptrs.len() as _,
-        enabled_extension_names: required_ext_ptrs.as_ptr(),
+        enabled_api_layer_count: required_layers.ptr.len() as _,
+        enabled_api_layer_names: required_layers.ptr.as_ptr(),
+        enabled_extension_count: required_extensions.ptr.len() as _,
+        enabled_extension_names: required_extensions.ptr.as_ptr(),
     };
 
     info!("xrCreateInstance()");
@@ -248,27 +227,30 @@ pub fn android_main() {
     info!("vulkan extensions: {:#?}", extensions);
 
     info!("xrGetVulkanInstanceExtensionsKHR()");
-    let mut count: u32 = 0;
-    let count_ptr: *mut u32 = &mut count;
-    let mut buffer = [0; 256];
-    let result = unsafe {
-        (fp.get_vulkan_instance_extensions_KHR)(
-            instance,
-            system_id,
-            256,
-            count_ptr,
-            buffer.as_mut_ptr(),
-        )
-    };
-    let req_extensions = &std::str::from_utf8(&buffer).unwrap()[..(count - 1) as usize];
-    let req_extensions: Vec<_> = req_extensions
-        .split_ascii_whitespace()
-        .map(|x| CString::new(x).unwrap())
-        .collect();
+    let req_extensions = {
+        let mut count: u32 = 0;
+        let count_ptr: *mut u32 = &mut count;
+        let mut buffer = [0; 256];
+        let result = unsafe {
+            (fp.get_vulkan_instance_extensions_KHR)(
+                instance,
+                system_id,
+                256,
+                count_ptr,
+                buffer.as_mut_ptr(),
+            )
+        };
 
-    if result != XrResult::SUCCESS {
-        panic!("Failed xrGetVulkanInstanceExtensionsKHR");
-    }
+        if result != XrResult::SUCCESS {
+            panic!("Failed xrGetVulkanInstanceExtensionsKHR");
+        }
+
+        let req_extensions = &std::str::from_utf8(&buffer).unwrap()[..(count - 1) as usize];
+        req_extensions
+            .split_ascii_whitespace()
+            .map(|x| CString::new(x).unwrap())
+            .collect::<Vec<_>>()
+    };
 
     info!("vulkan ext required: {:?}", req_extensions);
 
@@ -319,31 +301,63 @@ pub fn android_main() {
     let vk_instance_raw = vk_instance.handle().as_raw() as *const c_void;
 
     info!("xrGetVulkanGraphicsDeviceKHR()");
-    let mut physical_device = std::mem::MaybeUninit::new(std::ptr::null());
-    // TODO: Error handling
-    let result = unsafe {
-        (fp.get_vulkan_graphics_device_KHR)(
-            instance,
-            system_id,
-            vk_instance_raw,
-            physical_device.as_mut_ptr(),
-        )
+    let physical_device = {
+        let mut physical_device = std::mem::MaybeUninit::new(std::ptr::null());
+        // TODO: Error handling
+        let result = unsafe {
+            (fp.get_vulkan_graphics_device_KHR)(
+                instance,
+                system_id,
+                vk_instance_raw,
+                physical_device.as_mut_ptr(),
+            )
+        };
+
+        if result != XrResult::SUCCESS {
+            panic!("Failed xrGetVulkanGraphicsDeviceKHR");
+        }
+
+        let physical_device = unsafe { physical_device.assume_init() };
+        ash::vk::PhysicalDevice::from_raw(physical_device as u64)
+    };
+    info!("  physical_device: {:?}", physical_device);
+
+    info!("xrGetVulkanDeviceExtensionsKHR()");
+    let req_dev_extensions = {
+        let mut count: u32 = 0;
+        let mut buffer = [0; 256];
+        let result = unsafe {
+            (fp.get_vulkan_device_extensions_KHR)(
+                instance,
+                system_id,
+                256,
+                &mut count,
+                buffer.as_mut_ptr(),
+            )
+        };
+
+        if result != XrResult::SUCCESS {
+            panic!("Failed xrGetVulkanDeviceExtensionsKHR");
+        }
+
+        let req_dev_extensions = &std::str::from_utf8(&buffer).unwrap()[..(count - 1) as usize];
+        req_dev_extensions
+            .split_ascii_whitespace()
+            .map(|x| CString::new(x).unwrap())
+            .collect::<Vec<_>>()
     };
 
-    if result != XrResult::SUCCESS {
-        panic!("Failed xrGetVulkanGraphicsDeviceKHR");
-    }
+    info!("vulkan device ext required: {:?}", req_dev_extensions);
 
-    let physical_device = unsafe { physical_device.assume_init() };
-    let physical_device = ash::vk::PhysicalDevice::from_raw(physical_device as u64);
-
-    let device = std::ptr::null();
+    info!("create_logical_device()");
+    let (device, _queue) = create_logical_device(&vk_instance, physical_device);
+    info!("  device: {:?}", device.handle());
 
     let graphics_binding = openxr_sys::GraphicsBindingVulkanKHR {
         ty: openxr_sys::StructureType::GRAPHICS_BINDING_VULKAN_KHR,
         instance: vk_instance_raw,
         physical_device: physical_device.as_raw() as *const c_void,
-        device,
+        device: device.handle().as_raw() as *const c_void,
         queue_family_index: 0,
         queue_index: 0,
         next: std::ptr::null_mut(),
@@ -362,6 +376,115 @@ pub fn android_main() {
 
     if result != XrResult::SUCCESS {
         panic!("Failed xrCreateSession");
+    }
+}
+
+struct VecCStr {
+    ptr: Vec<*const u8>,
+    #[allow(dead_code)]
+    base: Vec<CString>,
+}
+
+fn to_veccstr(extensions: &[&str]) -> VecCStr {
+    let base = extensions
+        .iter()
+        .filter_map(|&name| CString::new(name).ok())
+        .collect::<Vec<_>>();
+
+    VecCStr {
+        ptr: base.iter().map(|layer| layer.as_ptr()).collect::<Vec<_>>(),
+        base,
+    }
+}
+
+fn find_queue_family(
+    instance: &ash::Instance,
+    physical_device: ash::vk::PhysicalDevice,
+) -> QueueFamilyIndices {
+    let queue_families =
+        unsafe { instance.get_physical_device_queue_family_properties(physical_device) };
+
+    let mut queue_family_indices = QueueFamilyIndices {
+        graphics_family: None,
+    };
+
+    let mut index = 0;
+    for queue_family in queue_families.iter() {
+        if queue_family.queue_count > 0
+            && queue_family
+                .queue_flags
+                .contains(ash::vk::QueueFlags::GRAPHICS)
+        {
+            queue_family_indices.graphics_family = Some(index);
+        }
+
+        if queue_family_indices.is_complete() {
+            break;
+        }
+
+        index += 1;
+    }
+
+    queue_family_indices
+}
+
+fn create_logical_device(
+    instance: &ash::Instance,
+    physical_device: ash::vk::PhysicalDevice,
+) -> (ash::Device, ash::vk::Queue) {
+    let indices = find_queue_family(instance, physical_device);
+
+    let queue_priorities = [1.0_f32];
+    let queue_create_info = ash::vk::DeviceQueueCreateInfo {
+        s_type: ash::vk::StructureType::DEVICE_QUEUE_CREATE_INFO,
+        p_next: std::ptr::null(),
+        flags: ash::vk::DeviceQueueCreateFlags::empty(),
+        queue_family_index: indices.graphics_family.unwrap(),
+        p_queue_priorities: queue_priorities.as_ptr(),
+        queue_count: queue_priorities.len() as u32,
+    };
+
+    let physical_device_features = ash::vk::PhysicalDeviceFeatures {
+        ..Default::default() // default just enable no feature.
+    };
+
+    let extensions = to_veccstr(&[
+        "VK_KHR_swapchain",
+        "VK_KHR_external_memory",
+        "VK_KHR_external_memory_fd",
+    ]);
+
+    let device_create_info = ash::vk::DeviceCreateInfo {
+        s_type: ash::vk::StructureType::DEVICE_CREATE_INFO,
+        p_next: std::ptr::null(),
+        flags: ash::vk::DeviceCreateFlags::empty(),
+        queue_create_info_count: 1,
+        p_queue_create_infos: &queue_create_info,
+        enabled_layer_count: 0,
+        pp_enabled_layer_names: std::ptr::null(),
+        enabled_extension_count: extensions.ptr.len() as u32,
+        pp_enabled_extension_names: extensions.ptr.as_ptr(),
+        p_enabled_features: &physical_device_features,
+    };
+
+    let device: ash::Device = unsafe {
+        instance
+            .create_device(physical_device, &device_create_info, None)
+            .expect("Failed to create logical Device!")
+    };
+
+    let graphics_queue = unsafe { device.get_device_queue(indices.graphics_family.unwrap(), 0) };
+
+    (device, graphics_queue)
+}
+
+struct QueueFamilyIndices {
+    graphics_family: Option<u32>,
+}
+
+impl QueueFamilyIndices {
+    pub fn is_complete(&self) -> bool {
+        self.graphics_family.is_some()
     }
 }
 
@@ -428,6 +551,7 @@ struct XrInstanceFp {
     get_vulkan_graphics_requirements_KHR: openxr_sys::pfn::GetVulkanGraphicsRequirementsKHR,
     get_vulkan_graphics_device_KHR: openxr_sys::pfn::GetVulkanGraphicsDeviceKHR,
     get_vulkan_instance_extensions_KHR: openxr_sys::pfn::GetVulkanInstanceExtensionsKHR,
+    get_vulkan_device_extensions_KHR: openxr_sys::pfn::GetVulkanDeviceExtensionsKHR,
     create_session: openxr_sys::pfn::CreateSession,
 }
 
@@ -443,6 +567,9 @@ impl XrInstanceFp {
                 ),
                 get_vulkan_instance_extensions_KHR: transmute(
                     fp.get_proc_addr(instance, "xrGetVulkanInstanceExtensionsKHR"),
+                ),
+                get_vulkan_device_extensions_KHR: transmute(
+                    fp.get_proc_addr(instance, "xrGetVulkanDeviceExtensionsKHR"),
                 ),
                 create_session: transmute(fp.get_proc_addr(instance, "xrCreateSession")),
             }
